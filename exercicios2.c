9d77ce9aca821fd9c27b52cfac4f940432f5088e
Rodrigo Mathias <104802552+seumathias@users.noreply.github.com>
2025-09-30 20:34:43 -0300

    Create exercicios2.c

---
+++ exercicios2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//Desenvolva um programa que gere e imprima vários números aleatórios entre
+//-100 e 100 (inclusos). O programa deve encerrar quando sortear o número 0.
+//Ao fim imprima a quantidade de números obtidos.
+int main(){
+	int random, cont=0;
+	srand(time(NULL));
+	do{
+		random = rand()%202;
+		printf(" %d\n", random-100);
+		cont++;
+		}
+	while(random != 0);
+	printf("Foram sorteados o total de %d\n", cont);
+	return 0;
+}
+
