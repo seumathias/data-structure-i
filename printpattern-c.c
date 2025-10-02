656bc03086a9957251b605597a8ae4ec179141e6
Rodrigo Mathias <104802552+seumathias@users.noreply.github.com>
2025-10-01 21:59:16 -0300

    Create printpattern-c.c

---
+++ printpattern-c.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    //TRIANGLE-1
+    int row =5;
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j <= i; j++){
+          printf("*"); 
+        }
+         printf("\n");
+    }
+    printf("\n\n\n\n");
+    
+    //TRIANGLE-1-INVERT
+    for (int i = row; i > 0; i++){
+        for (int j = 0; j >= i; j++){
+          printf("*"); 
+        }
+         printf("\n");
+    }
+  return 0;
+}
+
